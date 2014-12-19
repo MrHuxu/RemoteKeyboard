031112ebd6dc80a73e9c2a7ed8959d7fa457ba5f
胡旭 <xhu@koakiras-Mac-Pro.local>
2014-12-19 23:36:41 +0800

    add a history list

--- assets/js/index.js
+++ assets/js/index.js
@@ -17,10 +17,16 @@ $(function () {
     }
   });
 
+/*
   $("body").on("copy", "#copy", function(e) {
     e.clipboardData.clearData();
     e.clipboardData.setData("text/plain", $('#url').text());
     e.preventDefault();
     $('#modalMessage').text($('#url').text() + ' has been copied to the clipboard');
   });
+*/
+
+  $('#copy').click(function () {
+    $('#url_list').append("<li class='list-group-item'><h3>zhanqi.tv</h3></li>");
+  })
 });
