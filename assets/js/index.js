68fc09269ab1a8e171ebebfba59588290d6522f1
胡旭 <xhu@koakiras-Mac-Pro.local>
2014-12-28 02:19:09 +0800

    add the history module

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,13 +1,17 @@
 $(function () {
   var str = '';
   $('.letter').bind('click', function () {
+    if ($('#url').text() === '...')
+      str = '';
     str = str + $(this.children).text().toLowerCase();
     $('#url').text(str);
   });
+
   $('#clear').click(function () {
     str = '';
     $('#url').text('...');
   });
+  
   $('#back').click(function () {
     str = str.substring(0, str.length - 1);
     if (str.length === 0) {
@@ -17,16 +21,20 @@ $(function () {
     }
   });
 
-/*
   $("body").on("copy", "#copy", function(e) {
-    e.clipboardData.clearData();
-    e.clipboardData.setData("text/plain", $('#url').text());
-    e.preventDefault();
-    $('#modalMessage').text($('#url').text() + ' has been copied to the clipboard');
+    if ($('#url').text() != '...') {
+      e.clipboardData.clearData();
+      e.clipboardData.setData("text/plain", $('#url').text());
+      e.preventDefault();
+      $('#modalMessage').text($('#url').text() + ' has been copied to the clipboard');
+      for (var i = 4; i >= 1; i--) {
+        $($('#url_list').find('a')[i]).text($($('#url_list').find('a')[i - 1]).text());
+        $($('#url_list').find('a')[i]).attr('href', 'http://' + $($('#url_list').find('a')[i - 1]).text());
+      }
+      $($('#url_list').find('a')[0]).text($('#url').text());
+      $($('#url_list').find('a')[0]).attr('href', 'http://' + $('#url').text());
+      $('#url').text('...');
+    }
   });
-*/
 
-  $('#copy').click(function () {
-    $('#url_list').append("<li class='list-group-item'><h3>zhanqi.tv</h3></li>");
-  })
 });
